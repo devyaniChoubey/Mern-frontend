4fa0bce3bb9515369d343de40b50a8d086dc7f2b
Amartya Mondal <atm1504.in@gmail.com>
2021-11-10 16:48:57 +0530

    Fix axios

--- src/helpers/axios.js
+++ src/helpers/axios.js
@@ -1,11 +1,11 @@
 import axios from "axios";
-import {api} from '../urlConfig'
+import { api } from '../urlConfig'
 
-const axiosInstance = axios.instance({
-    baseURL : api
+const axiosInstance = axios.create({
+    baseURL: api
     // headers : {
     //     "Authorization": ""
     // }
-}) 
+})
 
 export default axiosInstance;
