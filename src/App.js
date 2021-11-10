d50a65cc828d770b295f78091cc0fd7e26579893
Amartya Mondal <atm1504.in@gmail.com>
2021-11-10 21:21:18 +0530

    Quick fix

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import './App.css';
-import {Route,Routes} from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Home from './containers/Home';
 import Signin from './containers/Signin/index';
 import Signup from './containers/Signup';
 import PrivateRoute from './components/HOC/PrivateRoute'
 
 function App() {
-  return(
+  return (
     <div className="App">
-       <Routes>
-        <PrivateRoute path="/" exact component={<Home />} />
-        <Route path="/signin" element={<Signin/>} />
+      <Routes>
+        {/* <PrivateRoute path="/" exact component={<Home />} /> */}
+        <Route path="/" exact component={<Home />} />
+        <Route path="/signin" element={<Signin />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
-     
+
     </div>
   )
 }
