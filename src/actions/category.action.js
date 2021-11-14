2325dc11b70a20e889f7070d62b435bef4122de7
devyani choubey <devyanichoubey16@gmail.com>
2021-11-14 19:51:00 +0530

    cat

--- src/actions/category.action.js
+++ src/actions/category.action.js
@@ -4,19 +4,19 @@ import { categoryConstants } from './constants';
 export const getAllCategory = () => {
     return async dispatch => {
 
-        dispatch({ type : categoryConstants.GET_ALL_CATEGORIES_REQUEST})
+        dispatch({ type: categoryConstants.GET_ALL_CATEGORIES_REQUEST })
         const res = await axios.get('/category/getcategory');
         console.log(res.data);
-        if(res.status === 200){
-            const {categoryList} = res.data;
+        if (res.status === 200) {
+            const { categoryList } = res.data;
             dispatch({
-                type : categoryConstants.GET_ALL_CATEGORIES_SUCCESS,
-                payload : {categories : categoryList}
+                type: categoryConstants.GET_ALL_CATEGORIES_SUCCESS,
+                payload: { categories: categoryList }
             })
-        }else{
+        } else {
             dispatch({
-                type : categoryConstants.GET_ALL_CATEGORIES_FAILURE,
-                payload : {error : res.data.error}
+                type: categoryConstants.GET_ALL_CATEGORIES_FAILURE,
+                payload: { error: res.data.error }
             })
         }
     }
@@ -25,13 +25,19 @@ export const getAllCategory = () => {
 
 export const addCategory = (form) => {
     return async dispatch => {
-        dispatch({ type : categoryConstants.ADD_NEW_CATEGORY_REQUEST})
+        dispatch({ type: categoryConstants.ADD_NEW_CATEGORY_REQUEST })
         const res = await axios.post('/category/create', form);
         console.log(res.data);
-        if(res.status === 200){
-            
-        }else{
-
+        if (res.status === 200) {
+            dispatch({
+                type: categoryConstants.ADD_NEW_CATEGORY_SUCCESS,
+                payload: {category : res.data.category}
+            })
+        } else {
+            dispatch({
+                type: categoryConstants.ADD_NEW_CATEGORY_FAILURE,
+                payload: res.data.error
+            })
         }
-    } 
+    }
 }
--- src/reducers/category.reducers.js
+++ src/reducers/category.reducers.js
@@ -7,6 +7,19 @@ const initState = {
     error: null
 }
 
+const buildNewCategories = (categories, category) => {
+    let myCategories  = [];
+
+    for(let cat of categories){
+        myCategories.push({
+            ...cat,
+            children: cat.children && cat.children.length > 0 ? buildNewCategories(cat.children, category) : []
+        })
+    }
+
+    return myCategories;
+}
+
 export default (state = initState, action) =>{
     switch(action.type){
         case categoryConstants.GET_ALL_CATEGORIES_SUCCESS:
@@ -26,6 +39,27 @@ export default (state = initState, action) =>{
             state = {
                 ...state,
                 loading : true
+            }
+            break;
+        case categoryConstants.ADD_NEW_CATEGORY_REQUEST:
+            state = {
+                ...state,
+                loading : true
+            }
+            break;
+        case categoryConstants.ADD_NEW_CATEGORY_SUCCESS:
+            const updatedCategories = buildNewCategories(state.categories, action.payload.category);
+            console.log(updatedCategories);
+            state = {
+                ...state,
+                loading : false
+
+            }
+            break;
+        case categoryConstants.ADD_NEW_CATEGORY_FAILURE:
+            state={
+               ...state,
+               loading : false
             }
             break;
     }
