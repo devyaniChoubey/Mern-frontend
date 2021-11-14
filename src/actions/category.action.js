ca5538164def768ac4b577277fc9d1442b0c6fdf
Amartya Mondal <atm1504.in@gmail.com>
2021-11-14 19:41:47 +0530

    Fix category actions

--- src/actions/category.action.js
+++ src/actions/category.action.js
@@ -1,22 +1,23 @@
-import axios from '../helpers/axios'
+import axiosInstance from '../helpers/axios'
 import { categoryConstants } from './constants';
+import axiosAdminInstance from "../helpers/axiosAdmin";
 
 export const getAllCategory = () => {
     return async dispatch => {
 
-        dispatch({ type : categoryConstants.GET_ALL_CATEGORIES_REQUEST})
-        const res = await axios.get('/category/getcategory');
+        dispatch({ type: categoryConstants.GET_ALL_CATEGORIES_REQUEST })
+        const res = await axiosInstance.get('/category/getcategory');
         console.log(res.data);
-        if(res.status === 200){
-            const {categoryList} = res.data;
+        if (res.status === 200) {
+            const { categoryList } = res.data;
             dispatch({
-                type : categoryConstants.GET_ALL_CATEGORIES_SUCCESS,
-                payload : {categories : categoryList}
+                type: categoryConstants.GET_ALL_CATEGORIES_SUCCESS,
+                payload: { categories: categoryList }
             })
-        }else{
+        } else {
             dispatch({
-                type : categoryConstants.GET_ALL_CATEGORIES_FAILURE,
-                payload : {error : res.data.error}
+                type: categoryConstants.GET_ALL_CATEGORIES_FAILURE,
+                payload: { error: res.data.error }
             })
         }
     }
@@ -25,13 +26,15 @@ export const getAllCategory = () => {
 
 export const addCategory = (form) => {
     return async dispatch => {
-        dispatch({ type : categoryConstants.ADD_NEW_CATEGORY_REQUEST})
-        const res = await axios.post('/category/create', form);
+        dispatch({ type: categoryConstants.ADD_NEW_CATEGORY_REQUEST })
+
+        const res = await axiosAdminInstance.post('http://localhost:2000/api/category/create', form);
+        console.log("Response is:", JSON.stringify(res))
         console.log(res.data);
-        if(res.status === 200){
-            
-        }else{
+        if (res.status === 200) {
+
+        } else {
 
         }
-    } 
+    }
 }
--- src/containers/Signin/index.js
+++ src/containers/Signin/index.js
@@ -1,41 +1,63 @@
-import Layout from '../../components/Layout/index'
+
+
+import React, { useState, useEffect } from 'react';
+import Layout from '../../components/Layout';
 import { Container, Form, Row, Col, Button } from 'react-bootstrap';
 import Input from '../../components/UI/Input';
-import {isUserLoggedIn, login} from '../../actions';
+import { login } from '../../actions';
 import { useDispatch, useSelector } from 'react-redux';
-import { useEffect, useState } from 'react';
-import { Navigate } from 'react-router';
+import { Navigate } from 'react-router-dom';
+
+/**
+* @author
+* @function Signin
+**/
 
 const Signin = (props) => {
 
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [error, setError] = useState('')
-    const auth = useSelector(state => state.auth)
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const auth = useSelector(state => state.auth);
 
     const dispatch = useDispatch();
 
-    const userLogin = (e) =>{
+    const userLogin = (e) => {
+
         e.preventDefault();
+
         const user = {
-            email,
-            password
+            email, password
         }
+
         dispatch(login(user));
     }
 
-    if(auth.authenticate){
-        return <Navigate to={'/'}/>
+    if (auth.authenticate) {
+        return <Navigate to={`/`} />
     }
-    
+
     return (
         <Layout>
             <Container>
-                <Row style={{marginTop: '50px'}}>
-                    <Col md={{span:6, offset: 3}}>
+                <Row style={{ marginTop: '50px' }}>
+                    <Col md={{ span: 6, offset: 3 }}>
                         <Form onSubmit={userLogin}>
-                            <Input label="Email" placeholder="Email" value={email} type="email" onChange={(e) => setEmail(e.target.value)} />
-                            <Input label="Password" placeholder="Password" value={password} type="text" onChange={(e) => setPassword(e.target.value)} />
+                            <Input
+                                label="Email"
+                                placeholder="Email"
+                                value={email}
+                                type="email"
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+
+                            <Input
+                                label="Password"
+                                placeholder="Password"
+                                value={password}
+                                type="password"
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
                             <Button variant="primary" type="submit">
                                 Submit
                             </Button>
@@ -47,4 +69,4 @@ const Signin = (props) => {
     )
 }
 
-export default Signin;
+export default Signin
